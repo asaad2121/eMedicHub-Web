dca6962211e910a9d44b6282d01af59a6ad728df
JoonaGeorge <joonageorge@gmail.com>
2025-08-28 20:05:09 +1200

    Make header sticky, disaply current user, and hide header when no medicines

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,6 +3,8 @@ import { NavigationEnd, Router, RouterOutlet } from "@angular/router";
 
 import { MatSnackBarModule } from "@angular/material/snack-bar";
 import { filter } from "rxjs";
+import { UserStreamService } from "./shared/services/user-stream.service";
+
 
 @Component({
   selector: "app-root",
@@ -11,8 +13,9 @@ import { filter } from "rxjs";
   styleUrl: "./app.less",
 })
 export class App {
-  showHeader = true;
-  constructor(private router: Router) {
+  showHeader = true;  
+  fullName : string = '';
+  constructor(private router: Router, private userStreamService:UserStreamService) {
     this.router.events
       .pipe(filter(event => event instanceof NavigationEnd))
       .subscribe((event: any) => {
@@ -21,6 +24,11 @@ export class App {
         // Hide header only if URL ends with '/login' or is root '/'
         this.showHeader = !(url === '/' || url.endsWith('/login'));
       });
+  }
+
+  ngOnInit() {
+    const storedUser = this.userStreamService.getCurrentUserFromStorage() as any;
+    this.fullName = [storedUser.first_name, storedUser.last_name].filter(name => name).join(' ');
   }
 }
 
