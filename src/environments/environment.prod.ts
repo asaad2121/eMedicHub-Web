aaeae4a7091a95f3e548078e94fd0cebec44f339
M Asaad Shaikh <Mohmmed Asaad Shaikh>
2025-09-08 21:14:28 +1200

    KAN-52 API error fixed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: "http://eMedicHub-ALB-858555586.us-east-1.elb.amazonaws.com",
+  apiUrl: "http://emedichub-alb-858555586.us-east-1.elb.amazonaws.com",
 };
